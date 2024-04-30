79c778176bbd23fc3e8c9c84024bf616fb4ff2c8
aniketbedarkar <aniketbedarkar1962@gmail.com>
2024-04-30 20:47:06 +0530

    clean up

--- Experimental/app/src/main/java/com/aniket/experimental/MainActivity.java
+++ Experimental/app/src/main/java/com/aniket/experimental/MainActivity.java
@@ -17,7 +17,6 @@ import androidx.core.view.WindowInsetsCompat;
 import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,35 +27,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show();
-        try {
-            Thread.sleep(5000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        imageView = findViewById(R.id.imageView);
-        GradientDrawable gradientDrawable = new GradientDrawable();
-
-        // Define the colors for the gradient
-        int[] colors = {Color.parseColor("#FF6F61"), Color.parseColor("#007EA7"), Color.parseColor("#00274C")};
-        gradientDrawable.setColors(colors);
-
-        // Set the gradient type to linear
-        gradientDrawable.setGradientType(GradientDrawable.LINEAR_GRADIENT);
-
-        for(GradientDrawable.Orientation orientation : GradientDrawable.Orientation.values()){
-            gradientDrawable.setOrientation(orientation);
-            imageView.setBackground(gradientDrawable);
-            try {
-                Thread.sleep(500L);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 }
